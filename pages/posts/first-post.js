c5742625d2761dac2ecac870a06dce381542d736
Oshioke-Salaki <salaki1902@gmail.com>
2023-10-16 18:00:52 -0600

    update

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,5 +1,33 @@
+import Link from 'next/link';
+import Head from 'next/head';
+import Image from 'next/image';
+import Script from 'next/script';
+import Layout from '../../components/layout';
 function FirstPost() {
-  return <div>first-post</div>;
+  return (
+    <Layout>
+      <Head>
+        <title>First Post</title>
+      </Head>
+      <Script
+        src="https://connect.facebook.net/en_US/sdk.js"
+        strategy="lazyOnload"
+        onLoad={() =>
+          console.log(`script loaded correctly, window.FB has been populated`)
+        }
+      />
+      <Image
+        src="/images/profile.jpg" // Route of the image file
+        height={144} // Desired size with correct aspect ratio
+        width={144} // Desired size with correct aspect ratio
+        alt="Your Name"
+      />
+      first-post
+      <h2>
+        <Link href="/">Back to home</Link>
+      </h2>
+    </Layout>
+  );
 }
 
 export default FirstPost;
